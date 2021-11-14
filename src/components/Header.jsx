e400004b9fd6a90e222b848fc227ccd1e5f18b67
Ary Barbosa <aryhertonbarbosa@gmail.com>
2021-11-14 19:52:31 -0300

    Requirement 02 and 04 >>> Ok <<<

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { getUser } from '../services/userAPI';
+import '../css/header.css';
+import Loading from './Loading';
+
 export default class Header extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      nome: '',
+      renderLoading: false,
+    };
+    this.userName = this.userName.bind(this);
+    this.selectRender = this.selectRender.bind(this);
+  }
+
+  componentDidMount() {
+    this.userName();
+  }
+
+  async userName() {
+    this.setState({ renderLoading: true });
+    const user = await getUser();
+    this.setState({ nome: user.name, renderLoading: false });
+  }
+
+  selectRender() {
+    const { renderLoading, nome } = this.state;
+    if (renderLoading) {
+      return <Loading />;
+    }
     return (
-      <header className="header">
+      <header className="header" data-testid="header-component">
         <div className="name-user">
+          <span data-testid="header-user-name">{ nome }</span>
           <nav>
-            <Link to="/Search">Pesquisar</Link>
-            <Link to="/Profile">Perfil</Link>
-            <Link to="/Favorites">Favorito</Link>
+            <ol>
+              <li>
+                <Link data-testid="link-to-search" to="/Search">Pesquisar</Link>
+              </li>
+              |
+              <li>
+                <Link data-testid="link-to-favorites" to="/Favorites">Favorito</Link>
+              </li>
+              |
+              <li>
+                <Link data-testid="link-to-profile" to="/Profile">Perfil</Link>
+              </li>
+              |
+              <li>
+                <Link to="/ProfileEdit">Perfil-Edit</Link>
+              </li>
+            </ol>
           </nav>
         </div>
       </header>
     );
+  }
+
+  render() {
+    return this.selectRender();
   }
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,71 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      nome: '',
+      checkRegister: false,
+      redir: false,
+    };
+
+    this.getInput = this.getInput.bind(this);
+    this.setUser = this.setUser.bind(this);
+    this.selectRender = this.selectRender.bind(this);
+  }
+
+  setUser() {
+    this.setState({ checkRegister: true });
+    const { nome } = this.state;
+    const num = 1000;
+    setTimeout(() => {
+      createUser({ name: nome, email: '', image: '', description: '' });
+      this.setState({ redir: true });
+      this.setState({ checkRegister: false });
+    }, num);
+  }
+
+  getInput(event) {
+    const valNome = event.target.value;
+    this.setState({ nome: valNome });
+  }
+
+  selectRender() {
+    const { nome } = this.state;
+    const lengthName = 4;
     return (
       <div data-testid="page-login" id="page-login">
         <h1>Login</h1>
         <form>
-          <input data-testid="login-name-input" type="text" placeholder="Nome" />
-          {/* <input type="password" placeholder="Senha" /> */}
-          <button data-testid="login-submit-button" type="button">Entrar</button>
+          <input
+            data-testid="login-name-input"
+            type="text"
+            placeholder="Nome"
+            onChange={ this.getInput }
+          />
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            disabled={ nome.length < lengthName }
+            onClick={ this.setUser }
+          >
+            Entrar
+          </button>
         </form>
       </div>
     );
+  }
+
+  render() {
+    const { checkRegister, redir } = this.state;
+    if (checkRegister) {
+      return <Loading />;
+    } if (redir) {
+      return <Redirect to="/search" />;
+    }
+    return this.selectRender();
   }
 }
