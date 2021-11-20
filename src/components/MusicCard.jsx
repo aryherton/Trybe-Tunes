452331ff3bab816b51e82852dbf679a61d0088d1
Ary Barbosa <aryhertonbarbosa@gmail.com>
2021-11-20 17:38:14 -0300

    Requirement 07 and 08 >>> Ok <<<

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,69 +1,35 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Loading from './Loading';
-import { addSong } from '../services/favoriteSongsAPI';
-import getMusics from '../services/musicsAPI';
 
 export default class MusicCard extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      loading: false,
-      favorite: [],
-      checkFavorite: false,
-    };
-
-    this.selectRender = this.selectRender.bind(this);
-    this.addFavotite = this.addFavotite.bind(this);
-    this.controlFavorite = this.controlFavorite.bind(this);
-    this.deleteFavorite = this.deleteFavorite.bind(this);
-  }
-
-  deleteFavorite(event) {
-    console.log('deletar '+event.target.value);
-  }
-
-  // Parei aqui os dados que a functino quer receber está na minha conversa com o tonis
-  async addFavotite(event) {
-    this.setState({ loading: true });
-    console.log(event.target.value);
-    // const { idSearch } = this.props;
-    // await addSong(getMusics(idSearch));
-  }
-
-  async controlFavorite(event) {
-    const { checkFavorite } = this.state;
-    await this.setState({ checkFavorite: !checkFavorite });
-    if (!checkFavorite) {
-      this.addFavotite(event);
-    } else {
-      this.deleteFavorite(event);
-    }
-  }
-
   selectRender() {
-    const { albuns, checkFavorite } = this.props;
-    if (albuns.length > 0) {
+    const {
+      albuns,
+      loading,
+      arrChecked,
+      controlFavorite } = this.props;
+    const [, ...arrNew] = albuns;
+    if (albuns.length > 0 && !loading) {
       return (
         <div>
           <div>
             <ul>
-              { albuns.map((album, keyMap) => (
+              { arrNew.map((album, keyMap) => (
                 <li key={ keyMap }>
                   <div>
                     <p data-testid="album-price">{ album.trackName }</p>
                     <label
                       htmlFor="input-favorite"
+                      value="Favorita"
                     >
-                      Favorita
                       <input
                         type="checkbox"
                         id="input-favorite"
                         value={ album.trackId }
                         data-testid={ `checkbox-music-${album.trackId}` }
-                        onClick={ this.controlFavorite }
-                        checked={ checkFavorite }
+                        onClick={ controlFavorite }
+                        checked={ arrChecked.includes(album.trackId.toString()) }
                       />
                     </label>
                     <audio
@@ -84,10 +50,8 @@ export default class MusicCard extends Component {
           </div>
         </div>
       );
-    } if (albuns.length === 0) {
-      return <Loading />;
     }
-    return <p>Nenhuma música a listar</p>;
+    return <Loading />;
   }
 
   render() {
@@ -96,5 +60,7 @@ export default class MusicCard extends Component {
 }
 
 MusicCard.propTypes = {
-  albuns: PropTypes.arrayOf(PropTypes.object).isRequired,
+  albuns: PropTypes.arrayOf(PropTypes.object),
+  loading: PropTypes.bool,
+  arrChecked: PropTypes.arrayOf(PropTypes.number),
 }.isRequired;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import getMusics from '../services/musicsAPI';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
 
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
@@ -14,10 +15,15 @@ export default class Album extends Component {
       albuns: [],
       artist: '',
       collection: '',
-      idSearch: '',
+      loading: false,
+      arrChecked: [],
     };
 
     this.getMusics = this.getMusics.bind(this);
+    this.selectRender = this.selectRender.bind(this);
+    this.addFavorite = this.addFavorite.bind(this);
+    this.controlFavorite = this.controlFavorite.bind(this);
+    this.deleteFavorite = this.deleteFavorite.bind(this);
   }
 
   componentDidMount() {
@@ -26,30 +32,68 @@ export default class Album extends Component {
 
   async getMusics() {
     const { match: { params: { id } } } = this.props;
-    await getMusics(id).then((promise) => {
-      const arrPromise = promise.filter((e) => e.previewUrl);
-      this.setState({
-        albuns: arrPromise,
-        artist: arrPromise[0].artistName,
-        collection: arrPromise[0].collectionName,
-        idSearch: id,
+
+    const arrPromise = await getMusics(id);
+    // arrPromise = arrPromise.filter((e) => e.previewUrl);
+    this.setState({
+      albuns: arrPromise,
+      artist: arrPromise[0].artistName,
+      collection: arrPromise[0].collectionName,
+    });
+  }
+
+  async controlFavorite(event) {
+    const { arrChecked } = this.state;
+
+    await this.setState({ loading: true });
+
+    if (!arrChecked.includes(event.target.value)) {
+      this.addFavorite(event);
+    } else {
+      this.deleteFavorite(event);
+    }
+  }
+
+  async addFavorite(event) {
+    const { arrChecked, albuns } = this.state;
+
+    arrChecked.push(event.target.value);
+    this.setState({ arrChecked }, async () => {
+      albuns.forEach(async (album) => {
+        if (album.trackId === Number(event.target.value)) {
+          await addSong(album);
+        }
       });
+      this.setState({ loading: false });
+    });
+  }
+
+  async deleteFavorite(event) {
+    const { arrChecked, albuns } = this.state;
+    const newArr = arrChecked.filter((item) => item !== event.target.value);
+
+    this.setState({ arrChecked: newArr }, async () => {
+      albuns.forEach(async (album) => {
+        if (album.trackId === Number(event.target.value)) { await removeSong(album); }
+      });
+      this.setState({ loading: false });
     });
   }
 
   selectRender() {
     const { albuns, artist, collection } = this.state;
-    if (albuns !== []) {
+    console.log(artist, collection);
+    if (albuns.length > 0) {
       return (
         <div data-testid="page-album" id="pageAlgum">
           <Header />
-          <p data-testid="artist-name">
-            { artist }
-          </p>
-          <p data-testid="album-name">
-            { collection }
-          </p>
-          <MusicCard { ...this.state } />
+          <p data-testid="artist-name">{artist}</p>
+          <p data-testid="album-name">{collection}</p>
+          <MusicCard
+            {
+              ...this.state }
+            controlFavorite={ this.controlFavorite }
+          />
         </div>
       );
     }
