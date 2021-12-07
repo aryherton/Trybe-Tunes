60c68e88c5d6d7f4d755ecfa4f2cddae034334c9
Ary Barbosa <aryhertonbarbosa@gmail.com>
2021-12-06 21:41:11 -0300

    Requirement 13 >>> Ok <<<

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
 
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      nameUser: '',
+      emailUser: '',
+      imgUser: '',
+      describeUser: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getInfoUser();
+  }
+
+  getInfoUser = async () => {
+    this.setState({ loading: true });
+    const arrInfoUser = await getUser();
+    console.log(arrInfoUser);
+    this.setState({
+      nameUser: arrInfoUser.name,
+      emailUser: arrInfoUser.email,
+      imgUser: arrInfoUser.image,
+      describeUser: arrInfoUser.description,
+      loading: false,
+    });
+  }
+
   render() {
+    const { loading, nameUser, emailUser, imgUser, describeUser } = this.state;
     return (
       <div data-testid="page-profile" id="pageProfile">
         <Header />
-        <h1>Profile</h1>
+        {loading
+          ? <Loading />
+          : (
+            <div>
+              <p>{ nameUser }</p>
+              <p>{ emailUser }</p>
+              <p>{ describeUser }</p>
+              <img data-testid="profile-image" src={ imgUser } alt="Imagem do usuário" />
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>
+          )}
       </div>
     );
   }
