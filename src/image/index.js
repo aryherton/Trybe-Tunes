ed3b6b1a83aedc646288ec22cfabafd925d274fe
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-29 17:44:09 -0300

    feat: created file "./src/image/index.js"

---
+++ src/image/index.js
@@ -0,0 +1,15 @@
+import logo01 from './logoTrybeTunes01.png';
+import logo02 from './logoTrybeTunes02.png';
+import gitDog from './gifRoxo.gif';
+import userPng from './user.png';
+import gifRoxo from './gifRoxo.gif';
+import mestreGif from './mestre-sound.gif';
+
+export {
+  logo01,
+  logo02,
+  gitDog,
+  userPng,
+  gifRoxo,
+  mestreGif,
+};
