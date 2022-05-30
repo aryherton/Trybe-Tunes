35c85efaf23f378c502fb0bec6de7f5127eac48e
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-05-30 12:13:04 -0300

    feat: created css for tag "main" in file "./src/pages/login"

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -8,19 +8,76 @@ export const LoginWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    position: relative;
 
-    h1, form, input, button {
+    main {
       background-color: transparent;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        border-radius: 50%;
+        background-color: transparent;
+        width: 250px;
+        height: 250px;
+        margin-right: 130px;
+      }
+
+      form {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 550px;
+        height: 250px;
+        background-color: transparent;
+        border-left: 5px solid rgba(255, 255, 255, 0.5);
+  
+        input {
+          background-color: yellow;
+          width: 350px;
+          height: 50px;
+          border-radius: 7px;
+          text-align: center;
+        }
+
+        button {
+          background-color: yellowgreen;
+          width: 250px;
+          height: 50px;
+          border-radius: 7px;
+          color: #fff;
+          font-size: 20px;
+          font-weight: bold;
+          border: 2px solid #fff;
+          cursor: pointer;
+        }
+
+        button:hover {
+          background-color: #fff;
+          color: #60703C;
+        }
+      }
     }
 
-    h1 {
-      text-align: center;
-    }
-
-    input {
-      background-color: yellowgreen;
-      width: 350px;
+    footer{
+      margin-top: 50px;
+      width: 100%;
       height: 50px;
-      border-radius: 7px;
+      display: flex;
+      justify-content: center;
+      align-content: center;
+      background-color: transparent;
+      position: absolute;
+      bottom: 70px;
+
+      img {
+        position: absolute;
+        right: 20px;
+        width: 120px;
+        height: 120px;
+        background-color: transparent;
+      }
     }
 `;
